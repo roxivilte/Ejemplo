8cbe99936ec11160fbb060b48c0dec88b81f9f83
Roxi <Roxi@roxipc.mshome.net>
2015-10-27 19:40:49 -0300

    ejemploo

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -15,8 +15,8 @@ public class Ejemplo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {}
+    System.out.println("Hola");
     }
     
 }
